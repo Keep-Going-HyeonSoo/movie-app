ad676798bbef5aa324e4bdb5ab1ea73a8febce2a
HyeonSoo Choi <chs98105@gmail.com>
2021-03-10 04:58:25 +0000

    feat: Sharing Props Between Routes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,30 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import styles from '../scss/Movie.module.scss'
 
 function Movie({
   year, title, summary, poster, genres
 }) {
   return (
-    <div className={styles.movie}>
-      <img className={styles.movie__poster} src={poster} alt={title} title={title} />
-      <h3 className={styles.movie__title}>{title} ({year})</h3>
-      <ul className={styles.movie__genres}>
-        {genres.map((genre) => (
-          <li
-            key={genre.toString()}
-            className={styles.movie__genres__genre}
-          >
-            {genre}
-          </li>
-        ))}
-      </ul>
-      {
-        summary.length > 200
-          ? <p className={styles.movie__summary}>{summary.slice(0, 200)}...</p>
-          : <p className={styles.movie__summary}>{summary}</p>
+    <Link to={{
+      pathname: '/detail',
+      state: {
+        year, title, summary, poster, genres
       }
-    </div>
+    }}
+    >
+      <div className={styles.movie}>
+        <img className={styles.movie__poster} src={poster} alt={title} title={title} />
+        <h3 className={styles.movie__title}>{title} ({year})</h3>
+        <ul className={styles.movie__genres}>
+          {genres.map((genre) => (
+            <li
+              key={genre.toString()}
+              className={styles.movie__genres__genre}
+            >
+              {genre}
+            </li>
+          ))}
+        </ul>
+        {
+          summary.length > 200
+            ? <p className={styles.movie__summary}>{summary.slice(0, 200)}...</p>
+            : <p className={styles.movie__summary}>{summary}</p>
+        }
+      </div>
+    </Link>
   )
 }
 
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-function About() {
+function About(props) {
+  console.log('props test: ', props)
   return (
     <span>
       About this page: I built it because I love movies.
